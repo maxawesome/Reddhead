f7b1ae6a94c48d868a7730739e362bb06df45d5b
Jason Baum <max.awesome@gmail.com>
2016-05-31 00:13:35 -0700

    Added a message on detail view load.

--- Reddhead/DetailViewController.swift
+++ Reddhead/DetailViewController.swift
@@ -32,6 +32,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Let's push some code, baby!")
         self.configureView()
     }
 
